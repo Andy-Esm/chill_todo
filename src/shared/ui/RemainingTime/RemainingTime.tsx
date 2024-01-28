61bb63b498dfb4ff4ead8ae37b4f22ece7375c29
Lebedev Anton <aslebedevdev@gmail.com>
2024-01-28 17:01:33 +0800

    Добавил функцию склонения слов

--- src/shared/ui/RemainingTime/RemainingTime.tsx
+++ src/shared/ui/RemainingTime/RemainingTime.tsx
@@ -1,3 +1,4 @@
+import { getDeclensions } from '@shared/lib/helpers/getDeclensions'
 import classNames from 'classnames'
 import moment, { Moment } from 'moment'
 import styles from './RemainingTime.module.scss'
@@ -7,33 +8,29 @@ interface RemainingTimeProps {
   date: Moment
 }
 
-const getRemainingString = (daysLeft: number) => {
-  const lastTwo = parseInt(daysLeft.toString().slice(-2))
-  const lastOne = parseInt(daysLeft.toString().slice(-1))
-
-  if (daysLeft === 0) {
-    return 'Срок истек'
+const getRemainingString = (daysLeft: number, timeIn: 'days' | 'hours') => {
+  const variants: Record<typeof timeIn, string[]> = {
+    days: ['день', 'дня', 'дней'],
+    hours: ['час', 'часа', 'часов'],
   }
-
-  switch (true) {
-    case daysLeft < 0:
-      return 'Срок истек'
-    case (lastOne === 1 && lastTwo < 10) || (lastTwo > 20 && lastOne === 1):
-      return `Остался ${daysLeft} день`
-    case (lastTwo < 10 && lastOne > 1 && lastOne <= 4) ||
-      (lastTwo > 20 && lastOne > 1 && lastOne <= 4):
-      return `Осталось ${daysLeft} дня`
-    default:
-      return `Осталось ${daysLeft} дней`
-  }
+  return daysLeft < 0
+    ? 'Срок истек'
+    : `${getDeclensions(daysLeft, ['Остался', 'Осталось', 'Осталось'])} ${daysLeft} ${getDeclensions(daysLeft, variants[timeIn])}`
 }
 
 export const RemainingTime = ({ className, date }: RemainingTimeProps) => {
   const remainingDays = date.diff(moment(), 'days')
+  const remainingHours = date.diff(moment(), 'hours')
   const remainingZero = remainingDays <= 0
   const remainingClass = classNames(styles['remaining-time'], className, {
     [styles['remaining-time-limit']]: remainingZero,
   })
 
-  return <span className={remainingClass}>{getRemainingString(remainingDays)}</span>
+  return (
+    <span className={remainingClass}>
+      {remainingDays === 0
+        ? getRemainingString(remainingHours, 'hours')
+        : getRemainingString(remainingDays, 'days')}
+    </span>
+  )
 }
