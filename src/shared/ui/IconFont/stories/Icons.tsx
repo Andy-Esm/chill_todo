c4f92b590e238599b2d2441b3afd90bf891a1c08
MSDipper <github5248@gmail.com>
2024-01-29 18:53:42 +0300

    обновил импорты стилей

--- src/shared/ui/IconFont/stories/Icons.tsx
+++ src/shared/ui/IconFont/stories/Icons.tsx
@@ -1,14 +1,14 @@
 import { Grid } from '@shared/ui/Grid'
 import { IconFont } from '../IconFont'
 import { ICON_NAMES } from '../constants'
-import style from './Icons.module.scss'
+import styles from './Icons.module.scss'
 
 export const Icons = () => (
   <Grid colMinWidth={90} columns='auto-fill' gap={8}>
     {ICON_NAMES.map((iconName) => (
       <Grid gap={2} justifyItems='center' key={iconName}>
         <IconFont iconName={`icon-${iconName}`} />
-        <span className={style.text}>{iconName}</span>
+        <span className={styles.text}>{iconName}</span>
       </Grid>
     ))}
   </Grid>
