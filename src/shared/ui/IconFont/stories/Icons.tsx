d6ca81e5c1309ddc9ed1afec409b4f66d87507f9
MSDipper <github5248@gmail.com>
2024-01-30 17:58:15 +0300

    исправил типизацию iconname

--- src/shared/ui/IconFont/stories/Icons.tsx
+++ src/shared/ui/IconFont/stories/Icons.tsx
@@ -1,17 +1,17 @@
-import { Grid } from '@shared/ui/Grid';
-import { IconFont } from '../IconFont';
-import icons from '../../../assets/style.module.scss';
+import { Grid } from '@shared/ui/Grid'
+import { IconFont, IconName } from '../IconFont'
+import icons from '../../../assets/style.module.scss'
 
 export const Icons = () => (
   <Grid colMinWidth={90} columns='auto-fill' gap={24}>
     {Object.keys(icons).map((iconName) => {
-      const iconNameWithoutPrefix = iconName.replace('icon-', '');
+      const iconNameWithoutPrefix = iconName.replace('icon-', '')
       return (
         <Grid gap={2} justifyItems='center' key={iconName}>
-          <IconFont iconName={iconName} />
+          <IconFont iconName={iconName as IconName} />
           <span>{iconNameWithoutPrefix}</span>
         </Grid>
-      );
+      )
     })}
   </Grid>
-);
+)
