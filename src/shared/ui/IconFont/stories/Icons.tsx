0266ac35bc648c64bf782873e8ff64ebef6d0d85
MSDipper <github5248@gmail.com>
2024-01-30 18:28:36 +0300

    отредактировал код

--- src/shared/ui/IconFont/stories/Icons.tsx
+++ src/shared/ui/IconFont/stories/Icons.tsx
@@ -2,16 +2,18 @@ import { Grid } from '@shared/ui/Grid'
 import { IconFont, IconName } from '../IconFont'
 import icons from '../../../assets/style.module.scss'
 
-export const Icons = () => (
-  <Grid colMinWidth={90} columns='auto-fill' gap={24}>
-    {Object.keys(icons).map((iconName) => {
-      const iconNameWithoutPrefix = iconName.replace('icon-', '')
-      return (
-        <Grid gap={2} justifyItems='center' key={iconName}>
-          <IconFont iconName={iconName as IconName} />
-          <span>{iconNameWithoutPrefix}</span>
-        </Grid>
-      )
-    })}
-  </Grid>
-)
+export const Icons = () => {
+  const iconNameWithoutPrefix = (iconName: string) => iconName.replace('icon-', '')
+  return (
+    <Grid colMinWidth={90} columns='auto-fill' gap={24}>
+      {Object.keys(icons).map((iconName) => {
+        return (
+          <Grid gap={2} justifyItems='center' key={iconName}>
+            <IconFont iconName={iconName as IconName} />
+            <span>{iconNameWithoutPrefix(iconName)}</span>
+          </Grid>
+        )
+      })}
+    </Grid>
+  )
+}
