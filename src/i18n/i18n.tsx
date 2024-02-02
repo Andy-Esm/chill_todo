b3adc134e67886f62d144889e37fcf93c247ef2c
MSDipper <github5248@gmail.com>
2024-02-02 12:39:35 +0300

    добавил кеширование в localstorage

--- src/i18n/i18n.tsx
+++ src/i18n/i18n.tsx
@@ -1,4 +1,5 @@
 import i18n from 'i18next'
+import localforage from 'localforage'
 
 const languages = ['en', 'ru', 'de']
 const namespaces = ['auth', 'menu']
@@ -6,9 +7,17 @@ const namespaces = ['auth', 'menu']
 const loadTranslations = async () => {
   for (const lng of languages) {
     for (const ns of namespaces) {
+      const cacheKey = `i18next_${lng}_${ns}`
+
       try {
-        const translations = await import(`../locales/${lng}/${ns}.json`)
-        i18n.addResourceBundle(lng, ns, translations.default)
+        const cachedTranslations = await localforage.getItem(cacheKey)
+        if (cachedTranslations) {
+          i18n.addResourceBundle(lng, ns, cachedTranslations)
+        } else {
+          const translations = await import(`../locales/${lng}/${ns}.json`)
+          i18n.addResourceBundle(lng, ns, translations.default)
+          await localforage.setItem(cacheKey, translations.default)
+        }
       } catch (error) {
         console.error(`Ошибка при загрузке переводов ${lng}/${ns}:`, error)
       }
